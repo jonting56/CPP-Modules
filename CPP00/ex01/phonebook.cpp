3f82efcfdec037899ccca7292459db071475cab8
Jonathan Ting <87597182+jonting56@users.noreply.github.com>
2022-07-22 16:47:55 +0930

    Add files via upload

--- CPP00/ex01/phonebook.cpp
+++ CPP00/ex01/phonebook.cpp
@@ -1,15 +1,26 @@
 #include "phonebook.hpp"
 
 
-void	add(int ac, char **av)
+void	add(int ac, char **av, PhoneBook p)
 {
-
+	std::cout << "Please enter a first name" << std::endl;
+	std::cin >> p.FirstName;
+	std::cout << "Please enter a last name" << std::endl;
+	std::cin >> p.LastName;
+	std::cout << "Please enter a nickname" << std::endl;
+	std::cin >> p.Nickname;
+	std::cout << "Please enter a phone number" << std::endl;
+	std::cin >> p.PhoneNum;
+	std::cout << "Please enter your darkest secret" << std::endl;
+	std::cin >> p.darkestSecret;
 }
 
 
 
 int main(int ac, char **av)
 {
-	phoneBook	instance;
+	PhoneBook	instance;
 	std::cout << "Please enter ADD, SEARCH or EXIT" <<std::endl;
+	std::cin <<
+	
 }
