2b72713e5804dc7d797b26621b5d5c443e1baf47
Jonathan Ting <87597182+jonting56@users.noreply.github.com>
2022-07-22 16:16:37 +0930

    First push

---
+++ CPP00/ex01/phonebook.cpp
@@ -0,0 +1,15 @@
+#include "phonebook.hpp"
+
+
+void	add(int ac, char **av)
+{
+
+}
+
+
+
+int main(int ac, char **av)
+{
+	phoneBook	instance;
+	std::cout << "Please enter ADD, SEARCH or EXIT" <<std::endl;
+}
---
+++ CPP00/ex01/phonebook.hpp
@@ -0,0 +1,22 @@
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+#include <iostream>
+
+class PhoneBook
+{
+	private:
+	std::string	FirstName;
+	std::string	LastName;
+	std::string	Nickname;
+	std::string	PhoneNum;
+	std::string	darkestSecret;
+
+	public:
+	int PhoneBook::add (int ac, char **av)
+	{
+
+	}
+};
+
+#endif
