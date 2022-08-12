adc2c51194f94a80e9d88dce48e29d511e600e48
Jonathan Ting <87597182+jonting56@users.noreply.github.com>
2022-08-12 22:25:53 +0930

    Update main.cpp

--- CPP02/ex00/main.cpp
+++ CPP02/ex00/main.cpp
@@ -5,12 +5,14 @@ int main(void)
 	Fixed a;
 	Fixed b(a);
 	Fixed c;
-
+	Fixed d;
+	
+	d.setRawBits(20);
 	c = b;
 
 	cout << a.getRawBits() << endl;
 	cout << b.getRawBits() << endl;
 	cout << c.getRawBits() << endl;
-
+	cout << d.getRawBits() << endl;
 	return 0;
 }
